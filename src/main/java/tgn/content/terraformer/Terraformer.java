a87b490b19ba54a571d0892a8eb558451bccdf83
Devan-Kerman <devan@cleverpath.com>
2020-01-07 16:40:33 -0600

    initial commit

--- src/main/java/tgn/content/terraformer/Terraformer.java
+++ src/main/java/tgn/content/terraformer/Terraformer.java
@@ -1,9 +1,9 @@
-package net.devtech.template;
+package tgn.content.terraformer;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Template extends JavaPlugin implements Listener {
+public final class Terraformer extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
